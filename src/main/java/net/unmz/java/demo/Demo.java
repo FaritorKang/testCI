c8bb47eef37048869026668a345f77a16010c4ad
Faritor <faritor@unmz.net>
2020-04-11 12:15:25 +0800

    test

---
+++ src/main/java/net/unmz/java/demo/Demo.java
@@ -0,0 +1,13 @@
+package net.unmz.java.demo;
+
+/**
+ * Project Name:
+ * 功能描述：
+ *
+ * @author faritor@unmz.net
+ * @version 1.0
+ * @date 2020-4-11 12:14
+ * @since JDK 1.8
+ */
+public class Demo {
+}
